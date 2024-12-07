312a9b7a4a1445dfc5f13da9867930293354d84b
mostafasaad700 <elmostafasaad561@gmail.com>
2024-12-07 23:53:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -16,12 +16,37 @@ for( var i=0; i<ceoList.length; i++)
         ceoTwo.classList.replace('d-flex' , 'd-none');
         
     })
-    
 }
 // subscribe//
+//navbar//
+let shopOffset = $('#shop').offset().top;
+$(window).scroll(function () { 
+    let wscroll = $(window).scrollTop();
+    if (wscroll > shopOffset) {
+        $('#mainNav').css('backgroundColor' , '#3b5d50de'); 
+        $('#btnUp').fadeIn(500);
+    }
+    else{
+        $('#mainNav').css('backgroundColor' , 'transparent');   
+        $('#btnUp').fadeOut(500);
+    }
+});
 
+    $('#btnUp').click(function () {
+        $(window).scrollTop(0);
+      })
+//loading screen//
+$(document).ready(function () {
+    $('#loading .spinner').fadeOut(2000 , function () {
+        $('#loading').fadeOut(1000 , function () {
+            $('#loading').remove();
+            $('body').css('overflow' , 'scroll')
+          });
+    });
+   
+    
 
-
+});
 
 
 
